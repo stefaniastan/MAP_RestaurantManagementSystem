d82a25fa4eef30304a036796cf7790b471b5f98d
stefaniastan <stefania.stan1@stud.ubbcluj.ro>
2025-10-27 13:56:39 +0200

    class MenuItem

--- src/main/java/com/example/map_proiect_restaurant/model/MenuItem.java
+++ src/main/java/com/example/map_proiect_restaurant/model/MenuItem.java
@@ -4,4 +4,31 @@ public class MenuItem {
     private String Id;
     private String Name;
     private double price;
+
+    public MenuItem(String id, String name, double price){
+        this.Id = Id;
+        this.Name = Name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return Id;
+    }
+    public void setId(String id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        this.Name = Name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
