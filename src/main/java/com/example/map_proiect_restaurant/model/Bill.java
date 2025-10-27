df82a3bd140e8f5d6e9dc046f64cccb30f41dae9
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-10-27 13:44:38 +0200

    bill ✅

--- src/main/java/com/example/map_proiect_restaurant/model/Bill.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Bill.java
@@ -4,4 +4,22 @@ public class Bill {
     private String Id;
     private String orderId;
     private double totalAmount;
+
+    public Bill(String id, String orderId, double totalAmount) {
+        this.Id = Id;
+        this.orderId = orderId;
+        this.totalAmount = totalAmount;
+
+    }
+
+    public String getId() {return Id;}
+    public void setId(String id) {this.Id = id;}
+
+    public String getOrderId() {return orderId;}
+    public void setOrderId(String orderId) {this.orderId = orderId;}
+
+    public double getTotalAmount() {return totalAmount;}
+    public void setTotalAmount(double totalAmount) {this.totalAmount = totalAmount;}
+
+
 }
