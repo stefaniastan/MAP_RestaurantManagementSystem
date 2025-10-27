d436b0f09c2c7febadfda71e73e9d6370a6bd339
stefaniastan <stefania.stan1@stud.ubbcluj.ro>
2025-10-27 13:30:40 +0200

    class Customer

--- src/main/java/com/example/map_proiect_restaurant/model/Customer.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Customer.java
@@ -6,4 +6,34 @@ public class Customer {
     private String Id;
     private String Name;
     private List<Order> orders;
+
+    public Customer(String Id, String Name, List<Order> orders) {
+        this.Id = Id;
+        this.Name = Name;
+        this.orders = orders;
+    }
+
+    public String getId(){
+        return Id;
+    }
+
+    public void setId(String Id){
+        this.Id = Id;
+    }
+
+    public String getName(){
+        return Name;
+    }
+
+    public void setName(String Name){
+        this.Name = Name;
+    }
+
+    public List<Order> getOrders(){
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders){
+        this.orders = orders;
+    }
 }
