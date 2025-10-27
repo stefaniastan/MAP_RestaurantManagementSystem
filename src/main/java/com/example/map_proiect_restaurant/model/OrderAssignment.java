2092d19a6294fc8d2b0423a09b982bb8b7008309
stefaniastan <stefania.stan1@stud.ubbcluj.ro>
2025-10-27 14:16:51 +0200

    class OrderAssignment

--- src/main/java/com/example/map_proiect_restaurant/model/OrderAssignment.java
+++ src/main/java/com/example/map_proiect_restaurant/model/OrderAssignment.java
@@ -4,4 +4,32 @@ public class OrderAssignment {
     private String Id;
     private String orderId;
     private String staffId;
+
+    public OrderAssignment(String Id, String orderId, String staffId) {
+        this.Id = Id;
+        this.orderId = orderId;
+        this.staffId = staffId;
+
+    }
+
+    public String getId() {
+        return Id;
+    }
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStaffId() {
+        return staffId;
+    }
+    public void setStaffId(String staffId) {
+        this.staffId = staffId;
+    }
 }
