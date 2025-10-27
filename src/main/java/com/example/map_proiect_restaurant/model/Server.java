d37134586366b6f91377006f7f5e8b1278cc0c6b
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-10-27 13:50:07 +0200

    server ✅

--- src/main/java/com/example/map_proiect_restaurant/model/Server.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Server.java
@@ -2,4 +2,15 @@ package com.example.map_proiect_restaurant.model;
 
 public class Server {
     private String designation;
+
+    public Server(String designation) {
+        this.designation = designation;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
 }
