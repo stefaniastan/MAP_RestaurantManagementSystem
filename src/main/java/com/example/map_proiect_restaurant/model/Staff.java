df8429f44d48c2213afa9b25a2c251779470d6bb
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-10-27 13:55:59 +0200

    staff ✅

--- src/main/java/com/example/map_proiect_restaurant/model/Staff.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Staff.java
@@ -3,4 +3,14 @@ package com.example.map_proiect_restaurant.model;
 public abstract class Staff {
     private String Id;
     private String Name;
+
+    public  Staff(String id, String name) {
+        this.Id = this.Id;
+        this.Name = this.Name;
+    }
+    public String getId() {return this.Id;}
+    public String setId() {return this.Id;}
+
+    public String getName() {return this.Name;}
+    public String setName() {return this.Name;}
 }
