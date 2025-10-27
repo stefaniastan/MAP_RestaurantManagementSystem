54a94235acb06642fd4c8b7f7f33ba63f020a442
stefaniastan <stefania.stan1@stud.ubbcluj.ro>
2025-10-27 13:47:09 +0200

    class Chef

--- src/main/java/com/example/map_proiect_restaurant/model/Chef.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Chef.java
@@ -2,4 +2,16 @@ package com.example.map_proiect_restaurant.model;
 
 public class Chef {
     private String specialization;
+
+    public Chef(String specialization){
+        this.specialization = specialization;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
 }
