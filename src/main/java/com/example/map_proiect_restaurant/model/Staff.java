170474c1676e31e7c41ebc99210d4c4cf7b88ac5
stefaniastan <stefania.stan1@stud.ubbcluj.ro>
2025-10-28 14:46:31 +0200

    atribut in plus la Staff

--- src/main/java/com/example/map_proiect_restaurant/model/Staff.java
+++ src/main/java/com/example/map_proiect_restaurant/model/Staff.java
@@ -3,14 +3,19 @@ package com.example.map_proiect_restaurant.model;
 public abstract class Staff {
     private String Id;
     private String Name;
+    private String Rating;
 
-    public  Staff(String id, String name) {
-        this.Id = id;
+    public  Staff(String Id, String name, String Rating) {
+        this.Id = Id;
         this.Name = name;
+        this.Rating = Rating;
     }
     public String getId() {return this.Id;}
-    public void setId(String id) {this.Id = id;}
+    public void setId(String Id) {this.Id = Id;}
 
     public String getName() {return this.Name;}
-    public void setName(String name) { this.Name = name;}
+    public void setName(String name) { this.Name = Name;}
+
+    public String getRating() {return this.Rating;}
+    public void setRating(String Rating) {this.Rating = Rating;}
 }
