71b98f7b92dc047ea453aa5e2836be636a16414e
Stoia Paul <vasile.stoia@stud.ubbcluj.ro>
2025-11-17 16:55:14 +0200

    Nu mai scriem ca maimutele localhost:8080/chefs /bills /customers , etc , intram direct pe home pe http://localhost:8080/

---
+++ src/main/java/com/example/map_proiect_restaurant/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.map_proiect_restaurant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home/index";
+    }
+}
